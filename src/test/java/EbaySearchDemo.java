1dd3e517beefab7406244a3ab43b2d6141e8a479
Thameera Malinga <djthameemalinga@gmail.com>
2024-10-26 18:58:42 +0530

    Script Modified

--- src/test/java/EbaySearchDemo.java
+++ src/test/java/EbaySearchDemo.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class EbaySearchDemo {
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class EbaySearchDemo {
+
+    WebDriver driver;
+
+    @BeforeTest
+    public void configEnv(){
+        driver = WebDriverManager.chromedriver().create();
+        driver.manage().window().maximize();
+        driver.get("https://www.ebay.com/");
+        //driver.navigate().to("https://www.ebay.com/");
+    }
+
+
+
+    @Test
+    public void iphoneSearch() throws InterruptedException {
+        driver.findElement(By.xpath("//input[@id='gh-ac']")).click();
+        driver.findElement(By.xpath("//input[@id='gh-ac']")).clear();
+        driver.findElement(By.xpath("//input[@id='gh-ac']")).sendKeys("iphone");
+        Thread.sleep(2000);
+        driver.findElement(By.id("gh-btn")).click();
+        Thread.sleep(2000);
+        
+    }
+
+    @AfterTest
+    public void endTest(){
+        driver.quit();
+    }
 }
